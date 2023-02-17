ab9e483b485762dd1b709b1f401de2fc63e07d62
Joseph <joejmitchell@hotmail.co.uk>
2023-02-17 21:37:20 +0000

    Fixed errors in database creation script

--- createdatabase.php
+++ createdatabase.php
@@ -1,5 +1,5 @@
 <?php
-    $connection = mysqli_connect("localhost", "root", "root") or die ("Connection Failed");
+    $connection = mysqli_connect("localhost", "root", "") or die ("Connection Failed");
 
     $action = "CREATE DATABASE codehunters";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
@@ -33,7 +33,7 @@
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*admin*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'admin@admin', 'Admin', \"" . password_hash("admin1", PASSWORD_DEFAULT) . "\", 6, 1)";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'admin@admin', 'Admin', \"" . password_hash("admin1", PASSWORD_DEFAULT) . "\", 6, 1)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
     
     $query = "SELECT id FROM profiledetails WHERE codename = 'Admin'";
@@ -45,7 +45,7 @@
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*aa*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'a@a', 'aa', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'a@a', 'aa', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'aa'";
@@ -53,11 +53,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 1, 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '2019-05-23 17:38:39', 'NULL', '2019-05-23 04:57:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-05-23 17:38:39', NULL, '2019-05-23 04:57:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*bb*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'b@b', 'bb', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'b@b', 'bb', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'bb'";
@@ -65,11 +65,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 4, 'NULL', '2019-05-23 17:46:53', 'NULL', '2019-05-23 17:47:05', 'NULL', '2019-05-23 17:46:46', 'NULL', '2019-05-23 17:53:37', 'NULL', '2019-05-23 04:58:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 4, NULL, '2019-05-23 17:46:53', NULL, '2019-05-23 17:47:05', NULL, '2019-05-23 17:46:46', NULL, '2019-05-23 17:53:37', NULL, '2019-05-23 04:58:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*cc*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'c@c', 'cc', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'c@c', 'cc', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'cc'";
@@ -77,11 +77,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 3, 'NULL', '2019-05-23 17:45:58', 'NULL', '2019-05-23 17:45:27', 'NULL', '2019-05-23 17:45:42', 'NULL', '2019-05-23 17:53:37', 'NULL', '2019-05-23 17:06:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 3, NULL, '2019-05-23 17:45:58', NULL, '2019-05-23 17:45:27', NULL, '2019-05-23 17:45:42', NULL, '2019-05-23 17:53:37', NULL, '2019-05-23 17:06:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*dd*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'd@d', 'dd', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'd@d', 'dd', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'dd'";
@@ -89,11 +89,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 8, '2019-05-23 18:26:44', 'NULL', '2019-05-23 18:27:10', '2019-05-23 18:26:38', '2019-05-23 18:27:16', '2019-05-23 18:26:50', '2019-05-23 18:27:04', '2019-05-23 18:26:56', '2019-05-23 18:27:20', '2019-05-23 18:27:20', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 8, '2019-05-23 18:26:44', NULL, '2019-05-23 18:27:10', '2019-05-23 18:26:38', '2019-05-23 18:27:16', '2019-05-23 18:26:50', '2019-05-23 18:27:04', '2019-05-23 18:26:56', '2019-05-23 18:27:20', '2019-05-23 18:27:20', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*ee*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'e@e', 'ee', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'e@e', 'ee', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'ee'";
@@ -101,11 +101,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 1, 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '2019-05-23 17:39:56', 'NULL', '2019-05-23 17:07:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-05-23 17:39:56', NULL, '2019-05-23 17:07:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*ff*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'f@f', 'ff', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'f@f', 'ff', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'ff'";
@@ -113,11 +113,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 5, 'NULL', 'NULL', '2019-05-23 18:03:43', '2019-05-23 18:03:19', '2019-05-23 18:03:35', '2019-05-23 18:03:25', 'NULL', 'NULL', '2019-05-23 18:03:50', '2019-05-23 17:07:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 5, NULL, NULL, '2019-05-23 18:03:43', '2019-05-23 18:03:19', '2019-05-23 18:03:35', '2019-05-23 18:03:25', NULL, NULL, '2019-05-23 18:03:50', '2019-05-23 17:07:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*gg*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'g@g', 'gg', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'g@g', 'gg', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'gg'";
@@ -125,11 +125,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 3, 'NULL', 'NULL', 'NULL', '2019-05-23 17:56:29', '2019-05-23 17:56:19', 'NULL', '2019-05-23 17:56:24', 'NULL', 'NULL', '2019-05-23 17:08:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 3, NULL, NULL, NULL, '2019-05-23 17:56:29', '2019-05-23 17:56:19', NULL, '2019-05-23 17:56:24', NULL, NULL, '2019-05-23 17:08:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*hh*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'h@h', 'hh', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'h@h', 'hh', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'hh'";
@@ -137,11 +137,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 3, 'NULL', 'NULL', 'NULL', '2019-05-23 17:56:29', '2019-05-23 17:56:19', 'NULL', '2019-05-23 17:56:24', 'NULL', 'NULL', '2019-05-23 17:08:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 3, NULL, NULL, NULL, '2019-05-23 17:56:29', '2019-05-23 17:56:19', NULL, '2019-05-23 17:56:24', NULL, NULL, '2019-05-23 17:08:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*ii*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'i@i', 'ii', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'i@i', 'ii', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 1, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'ii'";
@@ -149,11 +149,11 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 3, 'NULL', 'NULL', 'NULL', '2019-05-23 17:56:29', '2019-05-23 17:56:19', 'NULL', '2019-05-23 17:56:24', 'NULL', 'NULL', '2019-05-23 17:08:00', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 3, NULL, NULL, NULL, '2019-05-23 17:56:29', '2019-05-23 17:56:19', NULL, '2019-05-23 17:56:24', NULL, NULL, '2019-05-23 17:08:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     /*test*/
-    $action = "INSERT INTO profiledetails VALUES ('NULL', 'test@test', 'Test', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 7, 'NULL')";
+    $action = "INSERT INTO profiledetails VALUES (NULL, 'test@test', 'Test', \"" . password_hash("password", PASSWORD_DEFAULT) . "\", 7, 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 
     $query = "SELECT id FROM profiledetails WHERE codename = 'Test'";
@@ -161,6 +161,6 @@
     $row = mysqli_fetch_assoc($results);
     $id = $row["id"];
 
-    $action = "INSERT INTO foundcodes VALUES ('$id', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', 'NULL', '0', 'NULL')";
+    $action = "INSERT INTO foundcodes VALUES ('$id', 0, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, '2019-05-21 00:01:00', '0', 0)";
     mysqli_query($connection, $action) or die(mysqli_error($connection));
 ?>
